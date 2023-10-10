87030c7000353670642f6b51d8f98193538838a0
Entenado <fuciliabel@gmail.com>
2023-10-09 21:41:07 -0300

    Commit union back y front exitosa

---
+++ src/main/java/com/example/demo/personaRepo/personaRepositorio.java
@@ -0,0 +1,15 @@
+package com.example.demo.personaRepo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.entidades.Persona;
+
+@Repository
+public interface personaRepositorio extends JpaRepository<Persona, Long> {
+
+    @Query("SELECT u FROM Persona u WHERE u.nombre =:nombre")
+    public Persona buscarPorNombrePersona(@Param("nombre") String nombre);
+}
