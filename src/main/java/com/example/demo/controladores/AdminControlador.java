174385f554ac8dbdd0c0eff69d974a0d50001739
zamtty <gesagraterol@gmail.com>
2023-10-17 22:40:58 -0300

    version3

--- src/main/java/com/example/demo/controladores/AdminControlador.java
+++ src/main/java/com/example/demo/controladores/AdminControlador.java
@@ -1,4 +1,4 @@
-package com.example.demo.Controladores;
+package com.example.demo.controladores;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
