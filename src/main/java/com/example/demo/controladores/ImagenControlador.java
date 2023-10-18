568eaad670404c5b1267f60645c037afe3873024
mac753 <martin.correa.dvlpr@gmail.com>
2023-10-18 18:10:32 -0300

    Version aceptada

--- src/main/java/com/example/demo/controladores/ImagenControlador.java
+++ src/main/java/com/example/demo/controladores/ImagenControlador.java
@@ -1,4 +1,4 @@
-package com.example.demo.Controladores;
+package com.example.demo.controladores;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
