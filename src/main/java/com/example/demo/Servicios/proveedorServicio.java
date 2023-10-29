7ac481d09b93a0c1443c159271a71612de657e69
mac753 <martin.correa.dvlpr@gmail.com>
2023-10-28 21:24:46 -0300

    arreglo en proveedorServicio

--- src/main/java/com/example/demo/Servicios/proveedorServicio.java
+++ src/main/java/com/example/demo/Servicios/proveedorServicio.java
@@ -6,8 +6,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.example.demo.Enumeraciones.Rol;
@@ -17,10 +25,11 @@ import com.example.demo.Repositorio.proveedorRepositorio;
 import com.example.demo.entidades.Imagen;
 import com.example.demo.entidades.Proveedor;
 
+import jakarta.servlet.http.HttpSession;
 import jakarta.transaction.Transactional;
 
 @Service
-public class proveedorServicio {
+public class proveedorServicio implements UserDetailsService {
 
     @Autowired
     private proveedorRepositorio proveedorRepositorio;
@@ -165,7 +174,7 @@ public class proveedorServicio {
 
             ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
             HttpSession session = attr.getRequest().getSession(true);
-            session.setAttribute("personasession", proveedor);
+            session.setAttribute("usuariosession", proveedor);
             return new User(proveedor.getNombre(), proveedor.getPassword(), permisos);
 
         } else {
